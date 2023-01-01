e3722d1e7a5a6a4e87b7574010dbcde7232cdb2e
zheng-mingyu-azaan <zhengmingyu130624@126.com>
2023-01-01 11:34:47 +0800

    flo

--- blog4/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-    helper_method :current_user, :user_signed_in?
-
-    
-end
--- blog4/app/models/blog.rb
+++
@@ -1,5 +0,0 @@
-class Blog < ApplicationRecord
-    validates :title, :content,  presence: true
-    has_many :comments, dependent: :destroy
-    belongs_to :user
-end
